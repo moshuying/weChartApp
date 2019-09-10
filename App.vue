aeb4ce7f6f3c4b7207a8ad29d85f0097408b4e6a
moshuying <1460083332@qq.com>
2019-09-10 15:27:51 +0800

    构建项目

---
+++ App.vue
@@ -0,0 +1,24 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			// #ifdef APP-PLUS-NVUE
+			var domModule = weex.requireModule('dom');
+			domModule.addRule('fontFace', {
+				'fontFamily': "texticons",
+				'src': "url('./static/text-icon.ttf')"
+			});
+			// #endif
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
