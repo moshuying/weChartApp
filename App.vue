0f1ff4488a444f3a6a164df59e38c0f130c0f21c
moshuying <1460083332@qq.com>
2019-09-19 09:52:49 +0800

    完成数据请求,完善样式

--- App.vue
+++ App.vue
@@ -9,6 +9,28 @@ export default {
       src: "url('./static/text-icon.ttf')"
     });
     // #endif
+    uni.getSystemInfo({
+        success: function(e) {
+            // #ifndef MP
+            Vue.prototype.StatusBar = e.statusBarHeight;
+            if (e.platform == 'android') {
+                Vue.prototype.CustomBar = e.statusBarHeight + 50;
+            } else {
+                Vue.prototype.CustomBar = e.statusBarHeight + 45;
+            };
+            // #endif
+            // #ifdef MP-WEIXIN
+            Vue.prototype.StatusBar = e.statusBarHeight;
+            let custom = wx.getMenuButtonBoundingClientRect();
+            Vue.prototype.Custom = custom;
+            Vue.prototype.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+            // #endif       
+            // #ifdef MP-ALIPAY
+            Vue.prototype.StatusBar = e.statusBarHeight;
+            Vue.prototype.CustomBar = e.statusBarHeight + e.titleBarHeight;
+            // #endif
+        }
+    })
   },
   onShow: function() {
     console.log("App Show");
@@ -18,7 +40,8 @@ export default {
   }
 };
 </script>
-
+<style lang="scss">
+</style>
 <style>
 /*每个页面公共css */
 </style>
