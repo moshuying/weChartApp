d3ffdaf214da1a2912d62125b6e4631c38e214dc
moshuying <1460083332@qq.com>
2019-09-24 17:28:08 +0800

    完成注册界面

--- pages/miniTest/faceid/index.vue
+++ pages/miniTest/faceid/index.vue
@@ -1,11 +1,12 @@
 <template>
 	<view>
-		<camera device-position="back" flash="auto" @error="error" style="width: 100%; height: 500upx;">
+		<camera device-position="front" flash="auto" @error="error" style="width: 100%; height: 500upx;">
 			<cover-image src="../../../static/scan-frame/scan-img.png" class="scan-img"></cover-image>
 		</camera>
 		<view class="scan-text">请将XXX放置白色框内</view>
 		<button type="primary" @click="takePhoto">拍照</button>
-		<image mode="widthFix" class="photos-box" :src="src"></image>
+		<image mode="widthFix" class="photos-box" :src="src">{{res}}</image>
+    <view></view>
 	</view>
 </template>
 
@@ -13,7 +14,9 @@
 	export default {
 		data() {
 			return {
-				src: ""
+        src: "",
+        base64is:'',
+        res:'测试文本'
 			}
 		},
 		methods: {
@@ -28,12 +31,31 @@
               filePath:res.tempImagePath,
               encoding:'base64',
               success:(res)=>{
-              that.src = 'data:image/jpeg;base64,'+res.data
-              console.log(that.src)
+              this.src = 'data:image/jpeg;base64,'+res.data
+              this.base64is = res.data
+              console.log(this.base64is)
+              uni.request({
+                method:'POST',
+                header:{
+                'Content-Type':'application/json'
+                },
+                url:'https://aip.baidubce.com/rest/2.0/face/v3/faceset/user/add?access_token='+'24.f1fc6f1ba955d20800bc3c9b3fba3d0b.2592000.1571900694.282335-17329266',
+                data:{
+                  'image':this.base64is,
+                  'image_type':'BASE64',
+                  'group_id':'001',
+                  'user_id':'000',
+                  'user_info':'测试者'
+                },
+                success:(res)=>{
+                  this.res= JSON.stringify(res)
+                  console.log(res)
+                }
+              })
+
+              // console.log(this.src)
               }
             })
-            
-            
 						/* 返回调用页面并把图片URL传递过去 */
 						/* let pages = getCurrentPages();
 						let prevPage = pages[pages.length - 2]; 
@@ -49,11 +71,12 @@
 							console.log('获取图片：', currPage.data.image)
 						} */
 					}
-				});
+        });
 			},
 			error(e) {
 				console.log(e.detail);
-			}
+      }
+      
 		}
 	}
 </script>
